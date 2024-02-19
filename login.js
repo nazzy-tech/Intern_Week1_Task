056d9738c95f016e4de991f6acd8aef4974502ad
nazzy-tech <58662791+nazzy-tech@users.noreply.github.com>
2024-02-19 19:11:55 +0100

    Add files via upload

--- login.js
+++ login.js
@@ -9,15 +9,21 @@ formInput.addEventListener("submit", (e) => {
     let myUserName = document.getElementById("email");
     let myPassword = document.getElementById("password");
 
-    if (myUserName.value == "" || myPassword.value == "") {
-        alert("Please fillin a value in this field!!!");
-    } else if (myUserName.value == "" && myPassword.value == "") {
-        alert("You cannot submit an empty form, please input your username and password!!!");
-    } else {
-        openPopup;
+    let userNameError = document.getElementById("usernameError");
+    let passWordError = document.getElementById("passwordError");
 
-        myUserName.value == "";
-        myPassword.value == "";
+    if (myUserName.value == "") {
+        userNameError.innerHTML = "Required*"
+        return 0
+    }
+    if (myPassword.value == "") {
+        passWordError.innerHTML = "Required*"
+        return 0
+    } else {
+        openPopup();
+        formInput.reset()
+            // myUserName.value == "";
+            // myPassword.value == "";
 
     }
 
